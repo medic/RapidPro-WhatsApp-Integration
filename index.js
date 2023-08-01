c3a3e47c434e99df6a7cf2d1849028bd180b511c
femi oni <freefony@gmail.com>
2023-08-01 09:53:13 +0100

    Update index.js
    better error message

--- index.js
+++ index.js
@@ -46,8 +46,23 @@ app.post("/webhook", (req, res) => {
           }
       },
       headers: { "Content-Type": "application/json", Authorization: 'Bearer ' + token },
-    }).catch((err) => {
-        console.log("Error: There is an error in Post Req - " + err); 
+    }).catch((error) => {
+      if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+        console.error("Error: There is an error in Post Req - " + error); 
     });
 
   }

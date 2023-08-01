80460f3f1e1781292537fa417e46b5bf9e3e92b5
femi oni <freefony@gmail.com>
2023-08-01 11:40:57 +0100

    Update index.js
    more error fix

--- index.js
+++ index.js
@@ -53,14 +53,14 @@ app.post("/webhook", (req, res) => {
       // that falls out of the range of 2xx
       console.log(error.response.data);
       console.log(error.response.status);
-      console.log(error.response.headers);
+      
     } else if (error.request) {
       // The request was made but no response was received
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
       console.log(error.request);
     } 
-        console.error("Error: There is an error when sending data to facebook - ", data, error); 
+        console.error("Error: There is an error when sending data to facebook - ", data); 
     });
 
   }

c2a07120e94fce69c7b765776422cc0a2516fe9f
Femi Oni <freefony@gmail.com>
2023-10-01 22:58:20 +0100

    Co-authored-by: Femi Oni <freefony@gmail.com> Co-authored-by: Adnan Alhassan <aditech5170@gmail.com>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Imports dependencies and set up http server
 const { useV1Routes } = require("./src/routes");
 
-const createServer = require("./src/server");
+const { createServer } = require("./src/server");
 
 const app = useV1Routes();
 createServer(app);

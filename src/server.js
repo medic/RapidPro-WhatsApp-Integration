954fe021df5f26bc15e046e914152fc55fae4239
Femi Oni <freefony@gmail.com>
2023-10-01 22:53:44 +0100

    fix: module export

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ function createServer(app) {
   app.listen(process.env.PORT || 1337, () => console.log("webhook is listening on port", process.env.PORT || 1337));
 }
 
-exports.module = { createServer };
+module.exports = { createServer };
